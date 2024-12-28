72f60555f250622ff831e54d3eb8fcdf03520e16
Gapilesh Pirabaharan <108779095+Gapilesh@users.noreply.github.com>
2024-12-29 04:15:01 +0530

    Update Cat-and-Dog model training.py

--- Cat-and-Dog model training.py
+++ Cat-and-Dog model training.py
@@ -10,3 +10,12 @@ os.chdir('/content/drive/My Drive/Dataset/Kaggle/')
 # Extract the file
 !unzip dataset.zip
 
+#Importing Libraries
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
+import matplotlib.pyplot as plt
+import os
+
+#Prepare Dataset
