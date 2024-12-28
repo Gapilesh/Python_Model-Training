7454f323d4635496bdf923754f36a57f9ecba1cd
Gapilesh Pirabaharan <108779095+Gapilesh@users.noreply.github.com>
2024-12-29 04:10:44 +0530

    Update Cat-and-Dog model training.py

--- Cat-and-Dog model training.py
+++ Cat-and-Dog model training.py
@@ -1,1 +1,4 @@
-
+#Developing using Google Colab
+#Uploading the Dataset as compressed folder as .zip or .tar to google Drive (Uploading as one zip file is faster than uploading thousands of small files as a folder)
+from google.colab import drive
+drive.mount('/content/drive')
