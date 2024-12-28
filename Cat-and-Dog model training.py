1d794ff71257ebb8b9100cbcbb662a00629731f1
Gapilesh Pirabaharan <108779095+Gapilesh@users.noreply.github.com>
2024-12-29 04:11:41 +0530

    Update Cat-and-Dog model training.py

--- Cat-and-Dog model training.py
+++ Cat-and-Dog model training.py
@@ -2,3 +2,11 @@
 #Uploading the Dataset as compressed folder as .zip or .tar to google Drive (Uploading as one zip file is faster than uploading thousands of small files as a folder)
 from google.colab import drive
 drive.mount('/content/drive')
+
+# Change directory to where the compressed file is uploaded
+import os
+os.chdir('/content/drive/My Drive/Dataset/Kaggle/')
+
+# Extract the file
+!unzip dataset.zip
+
