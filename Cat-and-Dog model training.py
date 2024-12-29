2ccd4901d23e7376914aa9a55f2d74f45c7e4607
Gapilesh Pirabaharan <108779095+Gapilesh@users.noreply.github.com>
2024-12-29 12:51:58 +0530

    Update Cat-and-Dog model training.py

--- Cat-and-Dog model training.py
+++ Cat-and-Dog model training.py
@@ -36,3 +36,43 @@ train_generator = train_datagen.flow_from_directory(train_dir, target_size=IMG_S
 test_generator = test_datagen.flow_from_directory(test_dir, target_size=IMG_SIZE, batch_size=BATCH_SIZE, class_mode='binary')
 
 #Build CNN Model
+model = Sequential([
+    Conv2D(32, (3, 3), activation='relu', input_shape=(150, 150, 3)),
+    MaxPooling2D((2, 2)),
+    Conv2D(64, (3, 3), activation='relu'),
+    MaxPooling2D((2, 2)),
+    Conv2D(128, (3, 3), activation='relu'),
+    MaxPooling2D((2, 2)),
+    Flatten(),
+    Dense(128, activation='relu'),
+    Dropout(0.5),
+    Dense(1, activation='sigmoid')  # Binary classification
+])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
